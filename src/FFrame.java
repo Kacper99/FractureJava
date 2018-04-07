e7a13af1338dd58ddc496cab59d4b7b07b5e76c5
Kacper Martela <kacper99@hotmail.co.uk>
2018-04-07 18:51:45 +0100

    Made UI for game

--- src/FFrame.java
+++ src/FFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Window for the game and shows the UI
@@ -7,5 +8,26 @@ public class FFrame extends JFrame {
 
     public FFrame(String title) {
         super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+
+        Container c = this.getContentPane();
+        c.setLayout(new BorderLayout());
+
+        //Game scene
+        JPanel gamePanel = new JPanel(new GridBagLayout());
+        gamePanel.setBackground(Color.ORANGE);
+        JLabel challengeText = new JLabel("Challenge");
+        challengeText.setFont(new Font("font", Font.BOLD, 18));
+
+        //Add names button
+        JButton addNamesButton = new JButton("Add names");
+
+        gamePanel.add(challengeText);
+        c.add(gamePanel, BorderLayout.CENTER);
+        c.add(addNamesButton, BorderLayout.SOUTH);
+        
+        this.setVisible(true);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        FFrame window = new FFrame("Fracture");
     }
 }
