93b1df9a2964626e9732c45d1ee4d82b79a587e4
Kacper Martela <kacper99@hotmail.co.uk>
2018-04-07 18:31:02 +0100

    Created base classes

---
+++ src/FFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Window for the game and shows the UI
+ */
+public class FFrame extends JFrame {
+
+    public FFrame(String title) {
+        super(title);
+    }
+}
---
+++ src/Fracture.java
@@ -0,0 +1,15 @@
+/**
+ * Class used to run the game
+ */
+public class Fracture {
+
+    private int numQuestions;
+
+    public Fracture() {
+        loadQuestions();
+    }
+
+    public void loadQuestions() {
+
+    }
+}
